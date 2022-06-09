e5cb812853a25ab319b48e7ba6e83c639940f90b
AlanCYHsu <102496861+AlanCYHsu@users.noreply.github.com>
2022-06-09 17:41:44 +0800

    update main

---
+++ src/main/java/com/benq/BQAPartnerProtalAPI/BqaPartnerProtalApiApplication.java
@@ -0,0 +1,13 @@
+package com.benq.BQAPartnerProtalAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BqaPartnerProtalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BqaPartnerProtalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benq/BQAPartnerProtalAPI/BqaPartnerProtalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benq.BQAPartnerProtalAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BqaPartnerProtalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
